b5e13fc493c086555626256eb19f7ad36479d62a
yoosc89 <yoosc89@gmail.com>
2025-08-16 04:23:53 +0900

    초기 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
+import org.gradle.internal.declarativedsl.parsing.main
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
-    // id("org.graalvm.buildtools.native") version "0.10.6"  // 주석 처리
 }
 
 group = "com.habilisadi"
-version = "0.0.1"
+version = "0.0.0"
 description = "gateway"
 
 java {
@@ -24,15 +25,12 @@ extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    
-    // 최신 Gateway WebFlux 사용
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    
-    // 로드 밸런싱을 위한 추가 의존성
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
     
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -55,3 +53,4 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
